3753cec56a4bfde4a59448144d16827127876b6b
Andrea Miguel Jarillo <andreamj94@alumnos.cei.es>
2025-01-22 21:43:48 +0100

    commit
    completado hasta la actividad 7

--- Dia-2/actividad-2/index.js
+++ Dia-2/actividad-2/index.js
@@ -13,3 +13,46 @@ const ArtGallery = [
     { id: 10, artist: "Vermeer", title: "Girl with a Pearl Earring", year: 1665, isExhibited: true }
     ];
 
+console.log("Actividad 1");
+ArtGallery.forEach((obra) => {
+    const estado = obra.isExhibited ? "se encuentra exhibida" : "no encuentra exhibida";
+    console.log(`La obra "${obra.title}" por el autor ${obra.artist} (${obra.year}) ${estado}`);
+});
+
+console.log("Actividad 2");
+const titulos = ArtGallery.map((obra) => obra.title);
+console.log(titulos);
+
+console.log("Actividad 3");
+const exhibidas = ArtGallery.filter((obra)=>obra.isExhibited);
+console.log(exhibidas);
+
+console.log("Actividad 4");
+function buscarObra(title) {
+    const obra = ArtGallery.find((nombre) => nombre.title.toLowerCase() === title.toLowerCase());
+    return obra ? obra : "Obra no encontrada";
+}
+console.log(buscarObra("The kiss")); 
+
+console.log("Actividad 5");
+function yearObra(year) {
+    return ArtGallery.some((obra) => obra.year === year);
+}
+console.log(yearObra(1503));
+
+console.log("Actividad 6");
+function antesDeNacimiento(year) {
+    return ArtGallery.every((obra) => obra.year < year);
+}
+console.log(antesDeNacimiento(1994)); 
+
+console.log("Actividad 7");
+function posicionObra(title) {
+    const posicion = ArtGallery.findIndex((artwork) => artwork.title.toLowerCase() === title.toLowerCase());
+    return posicion !== -1 ? posicion : "Obra no encontrada";
+}
+console.log(posicionObra("mona lisa"));
+
+console.log("Actividad 8");
+
+console.log("Actividad 9");
