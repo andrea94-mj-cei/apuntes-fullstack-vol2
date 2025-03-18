017d171d22629a7c831ff23c5630feb146e89a7d
saragomezcei <gurutzefagoaga@alumnos.cei.es>
2025-03-18 18:57:11 +0100

    commit

--- Dia-34/auth-frontend/src/components/Registro.jsx
+++ Dia-34/auth-frontend/src/components/Registro.jsx
@@ -1,20 +1,62 @@
 import { useState } from "react";
 
 const Registro = () => {
+    const [nombre, setNombre] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    const [user, setUser] = useState([]);
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const nuevoUsuario = { nombre, email, password };
 
-    return ( 
+        try {
+            const response = await fetch(`${process.env.VITE_BACKEND}/auth/register`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(nuevoUsuario)
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                console.log('Usuario registrado:', data);
+                // Aquí puedes manejar el token si es necesario
+                // localStorage.setItem('jwtToken', data.token);
+            } else {
+                console.error('Error en el registro');
+            }
+        } catch (error) {
+            console.error('Error en la solicitud:', error);
+        }
+    };
+
+    return (
         <>
-        <h2>Registro</h2>
-        <form action="post">
-            <input type="text" placeholder="Nombre"/>
-            <input type="text" placeholder="Email"/>
-            <input type="password" placeholder="Contraseña"/>
-            <input type="submit" />
-        </form>
+            <h2>Registro</h2>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Nombre"
+                    value={nombre}
+                    onChange={(e) => setNombre(e.target.value)}
+                />
+                <input
+                    type="text"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Contraseña"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <input type="submit" />
+            </form>
         </>
-     );
-}
- 
+    );
+};
+
 export default Registro;
